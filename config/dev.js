61e0bdb02b186ddef902d2d250c581536a481b1b
Luis Osta <luisosta1999@gmail.com>
2019-05-11 10:36:44 -0500

    Added new mongo uri

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,8 @@
 module.exports = {
   googleClientID:
-    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
-  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
-  cookieKey: '123123123'
+    "964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com",
+  googleClientSecret: "KnH-rZC23z4fr2CN4ISK4srN",
+  mongoURI:
+    "mongodb://luisosta:o1011488@nodeblog-shard-00-00-ablf8.mongodb.net:27017,nodeblog-shard-00-01-ablf8.mongodb.net:27017,nodeblog-shard-00-02-ablf8.mongodb.net:27017/test?ssl=true&replicaSet=NodeBlog-shard-0&authSource=admin&retryWrites=true",
+  cookieKey: "123123123"
 };
