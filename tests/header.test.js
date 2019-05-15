32cb090f11b8df50925338156e7c3f1224573177
Luis Osta <luisosta1999@gmail.com>
2019-05-14 23:48:01 -0500

    expanded tests and added session and user factories

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,9 +1,11 @@
 const puppeteer = require("puppeteer");
-
+const sessionFactory = require("./factories/sessionFactories");
+const userFactory = require("./factories/userFactory");
 let browser;
 let page;
 
 beforeEach(async () => {
+  jest.setTimeout(30000);
   browser = await puppeteer.launch({
     headless: false
   });
@@ -17,7 +19,7 @@ afterEach(async () => {
   await browser.close();
 });
 
-test("The header has the correct", async () => {
+test("The header has the correct text", async () => {
   const text = await page.$eval("a.brand-logo", el => el.innerHTML);
 
   expect(text).toEqual("Blogster");
@@ -29,4 +31,25 @@ test("The user can login through google login", async () => {
   const url = await page.url();
 
   expect(url).toMatch(/accounts\.google\.com/);
+});
+
+test("When signed in, shows logout button", async () => {
+  // const id = "5cd6eb7533a4c7466aff5940";
+  const user = await userFactory();
+  const { session, sig } = sessionFactory(user);
+
+  await page.setCookie({ name: "session", value: session });
+  await page.setCookie({ name: "session.sig", value: sig });
+  await page.goto("localhost:3000");
+
+  //pupeteer will wait for the element to render
+  await page.waitFor("a[href='/auth/logout']");
+
+  //gets element
+  const logoutText = await page.$eval(
+    "a[href='/auth/logout']",
+    el => el.innerHTML
+  );
+
+  expect(logoutText).toEqual("Logout");
 });
