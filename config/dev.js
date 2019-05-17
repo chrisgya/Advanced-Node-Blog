cc6e42422cadeb71df2412307939f4af8dec4113
Luis Osta <luisosta1999@gmail.com>
2019-05-17 12:54:53 -0500

    added basic travis CI configs

--- config/dev.js
+++ config/dev.js
@@ -4,5 +4,6 @@ module.exports = {
   googleClientSecret: "KnH-rZC23z4fr2CN4ISK4srN",
   mongoURI:
     "mongodb://luisosta:o1011488@nodeblog-shard-00-00-ablf8.mongodb.net:27017,nodeblog-shard-00-01-ablf8.mongodb.net:27017,nodeblog-shard-00-02-ablf8.mongodb.net:27017/test?ssl=true&replicaSet=NodeBlog-shard-0&authSource=admin&retryWrites=true",
-  cookieKey: "123123123"
+  cookieKey: "123123123",
+  redisUrl: "redis://127.0.0.1:6379"
 };
